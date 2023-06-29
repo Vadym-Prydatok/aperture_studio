97ff47d551ee286c4614b209d8ec38f7e4d9951c
Vadym Prydatok <vadymprydatok@gmail.com>
2023-06-29 17:23:40 +0300

    add_subscribe

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -11,7 +11,7 @@ const modalWindow = document.querySelector("#modal-window");
 const closeButton = document.querySelector("#close-button");
 const submitButton = document.querySelector("#submit-button");
 const submitForms = document.querySelectorAll("#submit");
-const subscribeButton = document.querySelector("#subscribe-button");
+const subscribeButton = document.querySelector("#yes");
 
 submitForms.forEach(function (form) {
   form.addEventListener("submit", (e) => {
@@ -35,5 +35,7 @@ submitButton.addEventListener("click", () => {
 });
 
 subscribeButton.addEventListener("click", () => {
-  subscribeButton.parentElement.classList.toggle('subscribe__form--is-active');
+  modalWindow.style.display = "block";
+  modalWindow.firstElementChild.firstElementChild.textContent 
+  = 'Enter your email to receive news about us!'
 })
